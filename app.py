c04b8a2248dd209355a9bb4fbc54296cd29e67ad
Tony Cusumano <toncus2000@gmail.com>
2019-10-14 20:02:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def mydata():
         goodeed=GoodDeedsSignUp(fname=fname, lname=lname, myemail=myemail, password1=password1, password2=password2, mydate=mydate)
         db.session.add(goodeed)
         db.session.commit()
-        return redirect("http://localhost:5000/su", code=302)
+        return redirect('signUp.html', code=302)
 
 @app.route("/mission", methods=['GET', 'POST'])
 def mission():
