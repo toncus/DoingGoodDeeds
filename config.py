3ba066f3b9b1ee114db85c0d9146be2f95f187af
toncus <toncus2000@gmail.com>
2019-11-09 01:43:44 -0500

    add changes

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+import pandas as pd
+import numpy as np
+import random
+#MONGO_URI=os.environ.get('MONGO_URI')
+MONGODB_URI=os.environ.get('MONGODB_URI')
+#dbconn=os.environ.get('dbconn')
+print(MONGODB_URI)
