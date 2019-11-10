2fe611dc0658eccaeed09f64e0386e9eaf9f18ac
toncus <toncus2000@gmail.com>
2019-11-09 21:44:56 -0500

    add changes

--- app.py
+++ app.py
@@ -18,13 +18,18 @@ from flask_pymongo import PyMongo
 # Flask Setup
 #################################################
 app = Flask(__name__)
+if os.environ.get('MONGODB_URI'):
+  MONGODB_URI=os.environ.get('MONGODB_URI')
+else:
+  MONGODB_URI=ENV('MONGODB_URI')
+
 #MONGODB_URI=os.environ.get('MONGODB_URI')
 #MONGODB_URI=heroku config:get MONGODB_URI -a doinggoodness
 #dbconn=os.environ.get('dbconn')
 app.config["MONGO_DBNAME"] ="heroku_d68btb4j"
-#app.config["MONGO_URI"] = MONGODB_URI
+app.config["MONGO_URI"] = MONGODB_URI
 
-app.config["MONGO_URI"] = "mongodb://heroku_d68btb4j:uhnjf9kil1m6l9q8mktolb6mia@ds141198.mlab.com:41198/heroku_d68btb4j?retryWrites=false"
+#app.config["MONGO_URI"] = "mongodb://heroku_d68btb4j:uhnjf9kil1m6l9q8mktolb6mia@ds141198.mlab.com:41198/heroku_d68btb4j?retryWrites=false"
 mongo = PyMongo(app)
 
 #################################################
