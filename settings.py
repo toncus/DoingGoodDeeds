a5190b853eeabaafa58e98b29a50dea3834ccff2
toncus <toncus2000@gmail.com>
2019-11-12 11:43:48 -0500

    setting.py added

---
+++ settings.py
@@ -0,0 +1,14 @@
+# settings.py
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# OR, the same with increased verbosity:
+load_dotenv(verbose=True)
+
+# OR, explicitly providing path to '.env'
+from pathlib import Path  # python3 only
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+MONGODB_URI=os.getenv("MONGODB_URI")
